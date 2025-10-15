ab301610fef34a50eb119c3baa1db22eaf39e6b4
Mwia <iansnr10@gmail.com>
2025-10-15 06:49:32 +0300

    correcting git error

--- script.js
+++ script.js
@@ -16,12 +16,6 @@ expenseForm.addEventListener('submit', (e)=>{
     //total amount call
     addTotalExpense()
 
-    //add expense list call
-    addExpenseList()
-
-    //local storage
-    storeExpenses(expense.value, amount.value, category.value)
-
     //reset after user input
     expenseForm.reset()
 })
@@ -33,36 +27,3 @@ function addTotalExpense(){
     totalAmount += Number(amount.value)
     totalExpenses.innerHTML = `<h3> Total Amount: ${totalAmount}</h3>`
 }
-
-//append list items
-function addExpenseList(){
-    const expenseDescription = expense.value
-    const expenseAmount = amount.value
-    const expenseCategory = category.value
-
-    const expenseItem = document.createElement('div')
-
-    const buttonId = `remove-${Date.now()}`
-    
-    expenseItem.innerHTML = `<p>${expenseDescription}</p> -- ${expenseAmount} -- ${expenseCategory}
-    <button id='${buttonId}'>Remove</button>`
-
-    expenseList.appendChild(expenseItem)
-
-    document.getElementById(buttonId).addEventListener('click', ()=>{
-        expenseList.removeChild(expenseItem)
-        totalAmount -= Number(expenseAmount)
-        totalExpenses.innerHTML = `<h3> Total Amount: ${totalAmount}</h3>`
-        })
-        
-}
-//local storage
-function storeExpenses(description, amount, category){
-    const expenseItem = {description, amount, category}
-
-    let expenseItemList = JSON.parse(localStorage.getItem('expenseItems')) || []
-    expenseItemList.push(expenseItem)
-    localStorage.setItem('expenseItems', JSON.stringify(expenseItemList))
-}
-
-window.addEventListener('DOMContentLoaded', loadStoreExpenses)
