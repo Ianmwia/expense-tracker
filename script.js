760f0af6817e2abf01188a93fdb39f3f454cfe53
Mwia <iansnr10@gmail.com>
2025-10-15 06:52:05 +0300

    restoring previous code

--- script.js
+++ script.js
@@ -27,3 +27,36 @@ function addTotalExpense(){
     totalAmount += Number(amount.value)
     totalExpenses.innerHTML = `<h3> Total Amount: ${totalAmount}</h3>`
 }
+
+//append list items
+function addExpenseList(){
+    const expenseDescription = expense.value
+    const expenseAmount = amount.value
+    const expenseCategory = category.value
+
+    const expenseItem = document.createElement('div')
+
+    const buttonId = `remove-${Date.now()}`
+    
+    expenseItem.innerHTML = `<p>${expenseDescription}</p> -- ${expenseAmount} -- ${expenseCategory}
+    <button id='${buttonId}'>Remove</button>`
+
+    expenseList.appendChild(expenseItem)
+
+    document.getElementById(buttonId).addEventListener('click', ()=>{
+        expenseList.removeChild(expenseItem)
+        totalAmount -= Number(expenseAmount)
+        totalExpenses.innerHTML = `<h3> Total Amount: ${totalAmount}</h3>`
+        })
+        
+}
+//local storage
+function storeExpenses(description, amount, category){
+    const expenseItem = {description, amount, category}
+
+    let expenseItemList = JSON.parse(localStorage.getItem('expenseItems')) || []
+    expenseItemList.push(expenseItem)
+    localStorage.setItem('expenseItems', JSON.stringify(expenseItemList))
+}
+
+window.addEventListener('DOMContentLoaded')
