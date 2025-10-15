632dcad6fc73e1a81c908b39ab6d23282ca6795e
Mwia <iansnr10@gmail.com>
2025-10-15 05:48:33 +0300

    function to calculate the total expense

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const amount = document.getElementById('amount')
 const category = document.getElementById('category')
 const expenseForm = document.getElementById('expense-form')
 
+const expenseList = document.getElementById('expense-list')
+const filterCategory = document.getElementById('filter-category')
+const totalExpenses = document.getElementById('total-expenses')
+
+
 //event listener
 expenseForm.addEventListener('submit', (e)=>{
     e.preventDefault()
@@ -13,6 +18,17 @@ expenseForm.addEventListener('submit', (e)=>{
     const expenseAmount = amount.value
     const expenseCategory = category.value
 
+    //total amount call
+    addTotalExpense()
+
     //reset after user input
     expenseForm.reset()
 })
+
+//total amount calculator
+let totalAmount = 0
+
+function addTotalExpense(){
+    totalAmount += Number(amount.value)
+    totalExpenses.innerHTML = `<h3> Total Amount: ${totalAmount}</h3>`
+}
